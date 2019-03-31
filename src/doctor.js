de83ff7da882bac1ba8c4909f03f164a1a52d648
clarajmunro@gmail.com <clarajmunro@gmail.com>
2019-03-31 14:57:39 -0700

    name input retruns doctor details

--- src/doctor.js
+++ src/doctor.js
@@ -1,16 +1,28 @@
 let apiKey = process.env.exports.apiKey;
 
-//
 
 export default class Doctor {
-  constructor(inputName) {
+  constructor(inputName = '') {
     this.inputName = inputName;
   }
 
-  lookUpName()
-
-
+  lookUpName() {
+    return new Promise((resolve, reject) => {
+      let request = new XMLHttpRequest();
+      let location = "or-portland";
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=${location}&skip=0&limit=10&name=${this.inputName}&user_key=${apiKey}`;
+     
+      request.onload = function () {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+          alert('Error with the API request');
+        }
+      };
+      
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
 }
-
-
-   
--- src/main.js
+++ src/main.js
@@ -13,9 +13,42 @@ $(document).ready(function () {
     let inputName = $('#inputName').val();
     console.log(inputName);
 
+    let newDoc = new Doctor(inputName);
+
+    $('#doctorOutput').empty();
+    $('#errorOutput').empty();
+
+    let promise = newDoc.lookUpName();
+  
+    promise.then(function(response) {
+      let mainBody = JSON.parse(response);
+      console.log(mainBody);
+
+      let mainbodyData = mainBody.data;
+      console.log(mainbodyData);
+
+      if (mainbodyData.length === 0){
+        $('#doctorOutput').text('No doctor for this search input.');
+      } else {
+        for (let i = 0; i < mainbodyData.length; i++) {
+
+          // NAME
+          $('#doctorOutput').append(`<h2>${mainbodyData[i].profile.first_name} ${mainbodyData[i].profile.last_name} ${mainbodyData[i].profile.title}</h2>`);
+          
+          // ADDRESS
+          $('#doctorOutput').append(`<p><b>Address:</b> ${mainbodyData[i].practices[0].visit_address.street} ${mainbodyData[i].practices[0].visit_address.city} ${mainbodyData[i].practices[0].visit_address.state} ${mainbodyData[i].practices[0].visit_address.zip}</p>`);
+          
+          // PHONE
+          $('#doctorOutput').append(`<p><b>Phone:</b> ${mainbodyData[i].practices[0].phones[0].number}</p>`);
+          
+        }
+      }
+
+      // ERROR OUTPUT
+    }, function(error) {
+      $('#errorOutput').text(`Error processing your search: ${error.message}`);
+    });
   });
-
-
 
 
 
