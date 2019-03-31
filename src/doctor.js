2e30e8c41ad32b585df21ffe5ad0a3b2f04d55d4
clarajmunro@gmail.com <clarajmunro@gmail.com>
2019-03-31 13:16:50 -0700

    working on doctor.js

--- src/doctor.js
+++ src/doctor.js
@@ -1,9 +1,6 @@
 export default class Doctor {
-  constructor(year, month, day, expectancy) {
+  constructor(year) {
     this.year = year;
-    this.month = month;
-    this.day = day;
-    this.expectancy = expectancy;
   }
 
 }
