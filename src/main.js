66692b9d5a00f300971567e9465dad80d9a3709a
clarajmunro@gmail.com <clarajmunro@gmail.com>
2019-03-31 16:01:59 -0700

    added check for website & new patients- couldn't find any docs accepting new patients

--- src/main.js
+++ src/main.js
@@ -19,46 +19,50 @@ $(document).ready(function () {
     $('#errorOutput').empty();
 
     let promise = newDoc.lookUpName();
-  
-    promise.then(function(response) {
+
+    promise.then(function (response) {
       let mainBody = JSON.parse(response);
       console.log(mainBody);
 
       let mainbodyData = mainBody.data;
       console.log(mainbodyData);
 
-      if (mainbodyData.length === 0){
+      if (mainbodyData.length === 0) {
         $('#doctorOutput').text('No doctor for this search input.');
       } else {
         for (let i = 0; i < mainbodyData.length; i++) {
 
           // NAME
           $('#doctorOutput').append(`<h2>${mainbodyData[i].profile.first_name} ${mainbodyData[i].profile.last_name} ${mainbodyData[i].profile.title}</h2>`);
-          
+
           // ADDRESS
           $('#doctorOutput').append(`<p><b>Address:</b> ${mainbodyData[i].practices[0].visit_address.street} ${mainbodyData[i].practices[0].visit_address.city} ${mainbodyData[i].practices[0].visit_address.state} ${mainbodyData[i].practices[0].visit_address.zip}</p>`);
-          
+
           // PHONE
           $('#doctorOutput').append(`<p><b>Phone:</b> ${mainbodyData[i].practices[0].phones[0].number}</p>`);
-          
 
+          // IMAGE
           $('#doctorOutput').append(`<img src=${mainbodyData[i].profile.image_url}></img>`);
 
-          // CHECK WEBSITE --  IF STATEMENT
+          // CHECK NEW PATIENTS
+          if (`${mainbodyData[i].practices[0].accepts_new_patients}` === true) {
+            $('#doctorOutput').append('<p>Accepting New Patients</p>');
+          } else {
+            $('#doctorOutput').append('<p>Not Accepting New Patients</p>')
+          }
 
-
-          // CHECK ACCEPTING NEW PATIENTS -- IF STATEMENT
-
-
+          // CHECK FOR WEBSITE
+          if (`${mainbodyData[i].practices.website}` === 'undefined') {
+            $('#doctorOutput').append(`<p>No Website On File</p>`);
+          } else {
+            $('#doctorOutput').append(`<p>Website: ${mainbodyData[i].practices[0].website}</p>`);
+          }
         }
       }
-
-      // ERROR OUTPUT
-    }, function(error) {
+    }, function (error) {
       $('#errorOutput').text(`Error processing your search: ${error.message}`);
     });
   });
-
 
 
   // MEDICAL ISSUE AREA
