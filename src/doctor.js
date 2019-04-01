158f7921a64722d610157543bd8f80ce8a026088
clarajmunro@gmail.com <clarajmunro@gmail.com>
2019-03-31 19:42:31 -0700

    working on specialites section & some small styling features

--- src/doctor.js
+++ src/doctor.js
@@ -1,12 +1,15 @@
 let apiKey = process.env.exports.apiKey;
 
+// 1. test out no value for name
+// 2. test out no value for issue
 
 export default class Doctor {
-  constructor(inputName = '') {
+  constructor(inputName, inputIssue) {
     this.inputName = inputName;
+    this.inputIssue = inputIssue;
   }
 
-  lookUpName() {
+  lookUpByName() {
     return new Promise((resolve, reject) => {
       let request = new XMLHttpRequest();
       let location = "or-portland";
@@ -25,4 +28,37 @@ export default class Doctor {
       request.send();
     });
   }
+
+  // 3. figure out url for specialty search
+
+  lookUpByIssue() {
+    return new Promise((resolve, reject) => {
+      let request = new XMLHttpRequest();
+      let location = "or-portland";
+
+
+      // WHAT IS THE LOOKUP SPECIALTY URL?
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=${location}&skip=0&limit=10&name=${this.inputIssue}&user_key=${apiKey}`;
+     
+
+      request.onload = function () {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+          alert('Error with the API request');
+        }
+      };
+      
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+
+
+
+
+
+
+
 }
--- src/main.js
+++ src/main.js
@@ -15,10 +15,10 @@ $(document).ready(function () {
 
     let newDoc = new Doctor(inputName);
 
-    $('#doctorOutput').empty();
-    $('#errorOutput').empty();
+    $('#doctorOutputName').empty();
+    $('#errorOutputName').empty();
 
-    let promise = newDoc.lookUpName();
+    let promise = newDoc.lookUpByName();
 
     promise.then(function (response) {
       let mainBody = JSON.parse(response);
@@ -28,50 +28,75 @@ $(document).ready(function () {
       console.log(mainbodyData);
 
       if (mainbodyData.length === 0) {
-        $('#doctorOutput').text('No doctor for this search input.');
+        $('#doctorOutputName').text('No doctor for this search input.');
       } else {
         for (let i = 0; i < mainbodyData.length; i++) {
 
           // NAME
-          $('#doctorOutput').append(`<h2>${mainbodyData[i].profile.first_name} ${mainbodyData[i].profile.last_name} ${mainbodyData[i].profile.title}</h2>`);
+          $('#doctorOutputName').append(`<h2>${mainbodyData[i].profile.first_name} ${mainbodyData[i].profile.last_name} ${mainbodyData[i].profile.title}</h2>`);
 
           // ADDRESS
-          $('#doctorOutput').append(`<p><b>Address:</b> ${mainbodyData[i].practices[0].visit_address.street} ${mainbodyData[i].practices[0].visit_address.city} ${mainbodyData[i].practices[0].visit_address.state} ${mainbodyData[i].practices[0].visit_address.zip}</p>`);
+          $('#doctorOutputName').append(`<p><b>Address:</b> ${mainbodyData[i].practices[0].visit_address.street} ${mainbodyData[i].practices[0].visit_address.city} ${mainbodyData[i].practices[0].visit_address.state} ${mainbodyData[i].practices[0].visit_address.zip}</p>`);
 
           // PHONE
-          $('#doctorOutput').append(`<p><b>Phone:</b> ${mainbodyData[i].practices[0].phones[0].number}</p>`);
+          $('#doctorOutputName').append(`<p><b>Phone:</b> ${mainbodyData[i].practices[0].phones[0].number}</p>`);
 
           // IMAGE
-          $('#doctorOutput').append(`<img src=${mainbodyData[i].profile.image_url}></img>`);
+          $('#doctorOutputName').append(`<img src=${mainbodyData[i].profile.image_url}></img>`);
 
           // CHECK NEW PATIENTS
           if (`${mainbodyData[i].practices[0].accepts_new_patients}` === true) {
-            $('#doctorOutput').append('<p>Accepting New Patients</p>');
+            $('#doctorOutputName').append('<p>Accepting New Patients</p>');
           } else {
-            $('#doctorOutput').append('<p>Not Accepting New Patients</p>');
+            $('#doctorOutputName').append('<p>Not Accepting New Patients</p>');
           }
 
           // CHECK FOR WEBSITE
           if (`${mainbodyData[i].practices.website}` === 'undefined') {
-            $('#doctorOutput').append(`<p>No Website On File</p>`);
+            $('#doctorOutputName').append(`<p>No Website On File</p>`);
           } else {
-            $('#doctorOutput').append(`<p><b>Website:</b>${mainbodyData[i].practices[0].website}</p>`);
+            $('#doctorOutputName').append(`<p><b>Website:</b>${mainbodyData[i].practices[0].website}</p>`);
           }
         }
       }
     }, function (error) {
-      $('#errorOutput').text(`Error processing your search: ${error.message}`);
+      $('#errorOutputName').text(`Error processing your search: ${error.message}`);
     });
   });
 
 
-  // MEDICAL ISSUE AREA
 
+
+  // MEDICAL ISSUE AREA
   $('#formIssue').submit(function (event) {
     event.preventDefault();
 
     let inputIssue = $('#inputIssue').val();
 
+    let newDoc = new Doctor(inputIssue);
+
+    $('#doctorOutputIssue').empty();
+    $('#errorOutputIssue').empty();
+
+    let promise = newDoc.lookUpByIssue();
+
+    promise.then(function (response) {
+      let mainBody = JSON.parse(response);
+
+      let mainbodyData = mainBody.data;
+
+      if (mainbodyData.length === 0) {
+        $('#doctorOutputIssue').text('No doctor information for this medical input.');
+      } else {
+        for (let i = 0; i < mainbodyData.length; i++) {
+
+          // SPECIALTY INPUT FOR API?
+          $('#doctorOutputIssue').append(`<p>${mainbodyData.specialties}</p>`);
+        }
+      }
+    }, function (error) {
+      $('#errorOutputIssue').text(`Error processing your search: ${error.message}`);
+    });
   });
 
 
